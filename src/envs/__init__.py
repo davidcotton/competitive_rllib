3914bd7b0732cfe7c9c2333bf28da74d8cd0e42a
dave <davidcotton@users.noreply.github.com>
2019-08-20 17:57:32 +1000

    Add C4 game to init.py

--- src/envs/__init__.py
+++ src/envs/__init__.py
@@ -1,1 +1,1 @@
-from src.envs.connect4_env import Connect4Env, FlattenedConnect4Env, SquareConnect4Env
+from src.envs.connect4_env import Connect4, Connect4Env, FlattenedConnect4Env, SquareConnect4Env
