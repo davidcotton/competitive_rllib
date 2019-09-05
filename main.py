1488c664390cd9faaaf75465e8004749eba49f03
dave <davidcotton@users.noreply.github.com>
2019-09-05 13:45:07 +1000

    Update main.py with improved PPO hyperparams

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 import logging
+import random
 
 import ray
 from ray import tune
@@ -8,8 +9,7 @@ from ray.tune.registry import register_env
 
 from src.envs import Connect4Env, SquareConnect4Env
 from src.models import ParametricActionsMLP, ParametricActionsCNN
-from src.models.preprocessors import SquareObsPreprocessor, FlattenObsPreprocessor
-from src.policies import RandomPolicy
+from src.policies import HumanPolicy, RandomPolicy
 
 logger = logging.getLogger('ray.rllib')
 
@@ -20,17 +20,14 @@ if __name__ == '__main__':
     parser.add_argument("--use-cnn", action="store_true")
     parser.add_argument("--debug", action="store_true")
     args = parser.parse_args()
-
-    tune_config = {}
     ray.init(local_mode=args.debug)
-
-    # ModelCatalog.register_custom_preprocessor('flatten_obs_preprocessor', FlattenObsPreprocessor)
+    tune_config = {}
 
     if args.use_cnn:
         env_cls = SquareConnect4Env
-        ModelCatalog.register_custom_model('parametric_model', ParametricActionsCNN)
+        ModelCatalog.register_custom_model('parametric_actions_model', ParametricActionsCNN)
         model_config = {
-            'custom_model': 'parametric_model',
+            'custom_model': 'parametric_actions_model',
             'conv_filters': [[16, [2, 2], 1], [32, [2, 2], 1], [64, [3, 3], 2]],
             'conv_activation': 'leaky_relu',
             'fcnet_hiddens': [256, 256],
@@ -38,10 +35,11 @@ if __name__ == '__main__':
         }
     else:
         env_cls = Connect4Env
-        ModelCatalog.register_custom_model('parametric_model', ParametricActionsMLP)
+        ModelCatalog.register_custom_model('parametric_actions_model', ParametricActionsMLP)
         model_config = {
-            'custom_model': 'parametric_model',
-            'fcnet_hiddens': [256, 256],
+            'custom_model': 'parametric_actions_model',
+            # 'fcnet_hiddens': [256, 256],
+            'fcnet_hiddens': [128, 128],
             'fcnet_activation': 'leaky_relu',
         }
 
@@ -59,36 +57,70 @@ if __name__ == '__main__':
     if args.debug:
         tune_config['log_level'] = 'DEBUG'
 
+    policy = None
+    policies = ['learned', 'learned2']
+    # policies = ['learned', 'human']
+
+    def policy_mapping_fn(agent_id):
+        global policy
+        if agent_id == 0:
+            assert policy is None
+            policy = random.choice(policies)
+            return policy
+        else:
+            value = policies[0] if policy == policies[1] else policies[1]
+            policy = None
+            return value
+
     tune.run(
         args.policy,
         name='main',
         stop={
             # 'timesteps_total': int(500e3),
-            'timesteps_total': int(250e3),
-            'policy_reward_mean': {'learned': 0.99},
+            # 'timesteps_total': int(250e3),
+            # 'policy_reward_mean': {'learned': 0.99},
+            'policy_reward_mean': {'learned': 0.8, 'learned2': 0.8},
         },
         config=dict({
             'env': 'c4',
-            'gamma': 0.9,
+            'env_config': {},
+            # 'gamma': 0.9,
             # 'gamma': tune.grid_search([0.1, 0.3, 0.5, 0.7, 0.8, 0.9, 0.99, 0.999, 0.9999, 1.0]),
-            # 'num_workers': 20,
-            # 'num_workers': tune.grid_search([0, 1, 2, 4, 8, 16, 20]),
-            # 'num_envs_per_worker': 4,
             'num_workers': 0,
-            # 'num_envs_per_worker': 1,
+            'num_gpus': 0,
+            # 'num_workers': 20,
             # 'num_gpus': 1,
             # 'sample_batch_size': 10,
             # 'train_batch_size': 200,
+
+            # ------------------------
+            # PPO customisations
+            'lr': 0.001,
+            'clip_param': 0.2,
+            'gamma': 0.995,
+            'lambda': 0.95,
+            # 'kl_coeff': 1.0,
+            'train_batch_size': 65536,
+            'sgd_minibatch_size': 4096,
+            'num_sgd_iter': 6,
+            'num_envs_per_worker': 32,
+            # ------------------------
+
             'multiagent': {
                 'policies_to_train': ['learned'],
+                'policy_mapping_fn': tune.function(policy_mapping_fn),
+                # 'policy_mapping_fn': tune.function(lambda agent_id: ['learned', 'random'][agent_id % 2]),
+                # 'policy_mapping_fn': tune.function(lambda _: 'random'),
                 'policies': {
                     'random': (RandomPolicy, obs_space, action_space, {}),
                     'learned': (None, obs_space, action_space, {'model': model_config}),
                     'learned2': (None, obs_space, action_space, {'model': model_config}),
+                    'human': (HumanPolicy, obs_space, action_space, {}),
                 },
-                # 'policy_mapping_fn': tune.function(select_policy),
-                'policy_mapping_fn': tune.function(lambda agent_id: ['learned', 'random'][agent_id % 2]),
-                # 'policy_mapping_fn': tune.function(lambda agent_id: ['learned', 'learned2'][agent_id % 2]),
-                # 'policy_mapping_fn': tune.function(lambda _: 'random'),
             },
-        }, **tune_config))
+        }, **tune_config),
+        # checkpoint_freq=100,
+        # checkpoint_at_end=True,
+        # restore='/home/dave/ray_results_old/mcts_trainer/PPO_c4_0_2019-09-03_21-50-47nggyraoy/checkpoint_453/checkpoint-453',
+        # resume=True,
+    )
