c7258fd34b6fc3cfa9f20e779cc6989919be8216
dave <davidcotton@users.noreply.github.com>
2019-10-01 15:42:42 +1000

    Minor tidyup of win matrix callback

--- src/callbacks.py
+++ src/callbacks.py
@@ -16,9 +16,13 @@ def mcts_on_episode_end(info):
 
 
 def win_matrix_on_episode_end(info):
+    """Build a matrix to track in Tensorboard the win rate of agent pairs.
+
+    :param info: A dictionary of callback info.
+    """
     env = info['env'].envs[0]
-    agents = [int(agent[1][-2:]) for agent in info['episode'].agent_rewards.keys()]
-    agent_perms = [f'win({perm})' for perm in itertools.permutations(agents)]
+    agent_ids = [int(agent[1][-2:]) for agent in info['episode'].agent_rewards.keys()]
+    agent_perms = [f'win({perm})' for perm in itertools.permutations(agent_ids)]
     rewards = tuple(info['episode'].agent_rewards.values())
     for ids, reward in zip(agent_perms, rewards):
         if reward == env.reward_win:
