e2e5bbe2df0568384d9f4874cd67c8f4b91715ec
dave <davidcotton@users.noreply.github.com>
2019-10-09 12:39:06 +1100

    Add modified Episode

---
+++ modified_episode.py
@@ -0,0 +1,18 @@
+_DUMMY_AGENT_ID = 1
+
+
+def policy_for(self, agent_id=_DUMMY_AGENT_ID):
+    """Returns the policy for the specified agent.
+
+    If the agent is new, the policy mapping fn will be called to bind the
+    agent to a policy for the duration of the episode.
+    """
+
+    if self.episode_id not in self._agent_to_policy:
+        map_fn_data = {'episode_id': self.episode_id, 'env': self.user_data['env']}
+        self._agent_to_policy[self.episode_id] = self._policy_mapping_fn(map_fn_data)
+    return self._agent_to_policy[self.episode_id][agent_id]
+
+    # if agent_id not in self._agent_to_policy:
+    #     self._agent_to_policy[agent_id] = self._policy_mapping_fn(agent_id)
+    # return self._agent_to_policy[agent_id]
