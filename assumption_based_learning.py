5dcac5a7792a96fce55834276ef198a53788b047
dave <davidcotton@users.noreply.github.com>
2019-11-08 12:10:21 +1100

    Fix up bugs with assumption learner

--- assumption_based_learning.py
+++ assumption_based_learning.py
@@ -11,7 +11,6 @@ from ray.rllib.agents.ppo.ppo_policy import ppo_surrogate_loss, kl_and_loss_stat
 from ray.rllib.policy.sample_batch import SampleBatch
 from ray.rllib.policy.tf_policy_template import build_tf_policy
 from ray.rllib.utils import try_import_tf
-from ray.rllib.utils.tf_run_builder import TFRunBuilder
 
 from src.policies import HumanPolicy, RandomPolicy
 from src.utils import get_debug_config, get_learner_policy_configs, get_mcts_policy_configs, get_model_config
@@ -21,9 +20,6 @@ tf = try_import_tf()
 
 class AssistMixin:
     def __init__(self) -> None:
-        self.epsilon = 0.1
-        self.alpha = 1
-        self.gamma = 1
         self.memory = {}
 
     def compute_actions(self,
@@ -34,62 +30,87 @@ class AssistMixin:
                         info_batch=None,
                         episodes=None,
                         **kwargs):
-        limit = 0
-        level = 0
-        assist_actions = []
+        """Select actions to take from a batch of observations.
+
+        :param obs_batches: A batch of observations.
+        :param state_batches: A list of RNN state, if any.
+        :param prev_action_batch: Previous action values, if any.
+        :param prev_reward_batch: Previous reward, if any.
+        :param info_batch: Info object, if any.
+        :param episodes: The episode state.
+        :param kwargs:
+        :return: A tuple containing:
+            a list of actions to take,
+            a list of RNN state and,
+            a dict of info.
+        """
+
+        # 1) compute assisted actions
+        action_overrides = []
         for flattened_obs in obs_batches:
+            if flattened_obs[7] == 1:
+                action_overrides.append({})
+                continue  # skip other player's turn
             action_mask = flattened_obs[:8]
             obs = flattened_obs[8:50]
-
-            best_value = None
-            best_action = None
-            best_attempts = None
-            filtered_actions = []
-            for action, action_available in enumerate(action_mask):
+            action_override = {}
+            for action, action_available in enumerate(action_mask[:-1]):
                 if action_available == 0:
-                    continue  # skip invalid actions
+                    continue  # skip masked actions
                 value, attempts = self.get_value(obs, action)
-                if not(attempts < 0 and value < -0.99):
-                    filtered_actions.append(action)
-                if (best_value is None or value > best_value or (
-                        value == best_value and abs(attempts) > best_attempts)) and abs(attempts) > limit:
-                    best_value = value
-                    best_action = action
-                    best_attempts = abs(attempts)
-            assist_actions.append((filtered_actions, best_action))
+                if attempts < 0 and value < -0.99:
+                    flattened_obs[action] = 0  # mask the action
+                    print('assist masked action: %s, value: %s, attempts: %s' % (action, value, attempts))
+                if value > 0.999:
+                    reshaped_obs = obs.reshape((6, 7))
+                    action_override.update({
+                        'action': action,
+                        'value': value,
+                        'attempts': attempts,
+                        'obs': reshaped_obs
+                    })
+                    # action_override = action
+                    # print(reshaped_obs)
+                    # print('best_value', value, 'action_override', action, 'best_attempts', attempts)
+            action_overrides.append(action_override)
 
-            # if level == 0 and best_value != 1:
-            #     for py in range(6 - 4 + 1):
-            #         for px in range(7 - 4 + 1):
-            #             pass
+        # 2) compute neural network actions
+        fetches = super().compute_actions(obs_batches, state_batches, prev_action_batch, prev_reward_batch, info_batch,
+                                          episodes, **kwargs)
+        actions, state_outs, actions_info = fetches
 
-        actions = super().compute_actions(obs_batches, state_batches, prev_action_batch, prev_reward_batch, info_batch,
-                                      episodes, **kwargs)
-        # builder = TFRunBuilder(self._sess, "compute_actions")
-        # fetches = self._build_compute_actions(builder, obs_batch, state_batches, prev_action_batch, prev_reward_batch)
-        # actions = builder.get(fetches)
+        # 3) merge/compare assisted actions
+        actions_info['action_overridden'] = []
+        for i, action_override in enumerate(action_overrides):
+            # is_overridden = action_override is not None
+            is_overridden = 'action' in action_override
+            if is_overridden:
+                print('replaced action: %s with %s' % (actions[i], action_override['action']))
+                print(action_override['obs'])
+                print('value:', action_override['value'], 'attempts:', action_override['attempts'])
+                actions[i] = action_override['action']
+            actions_info['action_overridden'].append(int(is_overridden))
+        actions_info['action_overridden'] = np.array(actions_info['action_overridden'])
 
-        key = 'action_overridden'
-        actions[2][key] = []
-        for i in range(len(assist_actions)):
-            overridden = assist_actions[i][1] is not None
-            if overridden:
-                actions[0][i] = assist_actions[i][1]
-            actions[2][key].append(int(overridden))
-        actions[2][key] = np.array(actions[2][key])
+        return actions, state_outs, actions_info
 
-        return actions
-
-    def update_assist(self, sample_batch, other_agent_batches=None, episode=None):
-        foo = 1
+    def update_assist(self, sample_batch, other_agent_batches, episode):
+        if episode is None:  # RLlib startup, skip
+            return
+        steps = 0
+        reward = sample_batch[SampleBatch.REWARDS][-1]
         for i in range(sample_batch.count - 1, -1, -1):
             obs = sample_batch[SampleBatch.CUR_OBS][i]
+            if obs[7] == 1:
+                continue  # skip "pass action" (other player's turn)
+            obs = obs[8:50]
+            reshaped_obs = obs.reshape((6, 7))
             action = sample_batch[SampleBatch.ACTIONS][i]
-            reward = sample_batch[SampleBatch.REWARDS][i]
+            # reward = sample_batch[SampleBatch.REWARDS][i]
             value, attempts = self.get_value(obs, action)
             best_value = reward
 
-            if i == sample_batch.count - 1:
+            if steps == 0:
                 if attempts > 0:
                     attempts = 0
                 new_value = best_value - attempts * value
@@ -103,7 +124,8 @@ class AssistMixin:
                     new_value = 0
 
             self.memory[self.get_key(obs, action)] = new_value, attempts
-        return foo
+            reward = 0.0
+            steps += 1
 
     def get_key(self, obs, action):
         return tuple(obs), action
@@ -140,8 +162,7 @@ AssumptionPPOTFPolicy = build_tf_policy(
     before_init=ray.rllib.agents.ppo.ppo_policy.setup_config,
     before_loss_init=setup_mixins,
     mixins=[
-        LearningRateSchedule, EntropyCoeffSchedule, KLCoeffMixin,
-        ValueNetworkMixin,
+        LearningRateSchedule, EntropyCoeffSchedule, KLCoeffMixin, ValueNetworkMixin,
         AssistMixin
     ])
 
@@ -151,7 +172,7 @@ if __name__ == '__main__':
     parser.add_argument('--policy', type=str, default='PPO')
     parser.add_argument('--use-cnn', action='store_true')
     parser.add_argument('--num-learners', type=int, default=2)
-    parser.add_argument('--assist', action='store_false')
+    parser.add_argument('--assist', action='store_true')
     parser.add_argument('--mcts', type=int, default=0)
     # e.g. --restore="/home/dave/ray_results/main/PPO_c4_0_2019-09-23_16-17-45z9x1oc9j/checkpoint_782/checkpoint-782"
     parser.add_argument('--restore', type=str)
@@ -168,50 +189,59 @@ if __name__ == '__main__':
     obs_space, action_space = env.observation_space, env.action_space
 
     num_learners = 1 if args.human or args.mcts > 0 else args.num_learners
-    learner_policy = AssumptionPPOTFPolicy if args.assist else None
-    trainable_policies = {
-        f'learned{i:02d}': (learner_policy, obs_space, action_space, {'model': model_config}) for i in
-        range(num_learners)}
-    mcts_policies = get_mcts_policy_configs([8, 16, 32, 64, 128, 256, 512], obs_space, action_space)
-    stop_criterion = ('policy_reward_mean/learned00', 0.6) if args.mcts > 0 else ('timesteps_total', int(100e6))
-
-    if args.policy == 'DQN':
+    learner_policy = None
+    if args.policy == 'PPO':
+        tune_config.update({
+            'lr': 0.001,
+            'gamma': 0.995,
+            'clip_param': 0.2,
+            'lambda': 0.95,
+            'kl_coeff': 1.0,
+        })
+        if args.assist:
+            print('Using assumption-based learning policy: AssumptionPPOTFPolicy')
+            learner_policy = AssumptionPPOTFPolicy
+    elif args.policy == 'DQN':
         tune_config.update({
             'hiddens': [],
             'dueling': False,
         })
+        if args.assist:
+            raise ValueError('DQN assist not implemented yet')
+    trainable_policies = {
+        f'learned{i:02d}': (learner_policy, obs_space, action_space, {'model': model_config}) for i in
+        range(num_learners)}
+    mcts_policies = get_mcts_policy_configs([2, 8, 16, 32, 64, 128, 256, 512], obs_space, action_space)
+    stop_criterion = ('policy_reward_mean/learned00', 0.6) if args.mcts > 0 else ('timesteps_total', int(100e6))
 
     def random_policy_mapping_fn(info):
         if args.human:
             return random.sample(['learned00', 'human'], k=2)
         elif args.mcts > 0:
             return random.sample(['learned00', f'mcts{args.mcts:03d}'], k=2)
-        elif args.num_learners == 1:
+        elif num_learners == 1:
             return ['learned00', 'learned00']
         else:
             return random.sample([*trainable_policies], k=2)
 
     def name_trial(trial):
         """Give trials a more readable name in terminal & Tensorboard."""
-        return f'{args.num_learners}x{trial.trainable_name}'
+        assist = 'Assist' if args.assist else 'NoAssist'
+        mcts = f'vsMCTS{args.mcts}' if args.mcts > 0 else ''
+        debug = '-debug' if args.debug else ''
+        return f'{num_learners}x{trial.trainable_name}{mcts}-{assist}{debug}'
 
     tune.run(
-        # args.policy,
-        'PPO',
-        name='main',
+        args.policy,
+        name='ABL',
         trial_name_creator=name_trial,
         stop={
             stop_criterion[0]: stop_criterion[1]
-            # 'timesteps_total': int(100e6),
         },
         config=dict({
             'env': 'c4',
-            'env_config': {},
             'lr': 0.001,
             'gamma': 0.995,
-            'clip_param': 0.2,
-            'lambda': 0.95,
-            'kl_coeff': 1.0,
             'multiagent': {
                 'policies_to_train': [*trainable_policies],
                 'policy_mapping_fn': random_policy_mapping_fn,
